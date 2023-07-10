7b177a64948b8064a137e54aeec8a637fbe927e2
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 07:16:45 +0700

    addpg

--- index.php
+++ index.php
@@ -12,13 +12,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration System PDO</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="index.css">
+<link rel="stylesheet" href="anm.js">
 </head>
 <body>
-
-    <div class="container">
-        <h3 class="mt-4">สมัครสมาชิก</h3>
-        <hr>
+<div class="container">
         <form action="signup_db.php" method="post">
             <?php if(isset($_SESSION['error'])) { ?>
                 <div class="alert alert-danger" role="alert">
@@ -45,31 +43,21 @@
                 </div>
             <?php } ?>
 
-            <div class="mb-3">
-                <label for="firstname" class="form-label">First name</label>
-                <input type="text" class="form-control" name="firstname" aria-describedby="firstname">
-            </div>
-            <div class="mb-3">
-                <label for="lastname" class="form-label">Last name</label>
-                <input type="text" class="form-control" name="lastname" aria-describedby="lastname">
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" name="email" aria-describedby="email">
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" name="password">
-            </div>
-            <div class="mb-3">
-                <label for="confirm password" class="form-label">Confirm Password</label>
-                <input type="password" class="form-control" name="c_password">
-            </div>
-            <button type="submit" name="signup" class="btn btn-primary">Sign Up</button>
-        </form>
-        <hr>
-        <p>เป็นสมาชิกแล้วใช่ไหม คลิ๊กที่นี่เพื่อ <a href="signin.php">เข้าสู่ระบบ</a></p>
+			
+  <form id="form" autocomplete="off">
+    <h1 id="message">Get Started</h1><small id="smallMessage"> </small>
+    <div class="field">
+      <input id="email" type="email" name="email" placeholder="Email" autocomplete="off"/>
+      <label for="email">Email</label>
     </div>
-    
+    <div class="field">
+      <input id="password" type="password" name="password" placeholder="Password" autocomplete="new-password"/>
+      <label for="password">Password</label>
+    </div>
+    <button id="submit">Login</button>
+    <p>By signing up, I agree to to the Terms of Service and Privacy Policy</p>
+  </form>
+</div>
+
 </body>
 </html>
